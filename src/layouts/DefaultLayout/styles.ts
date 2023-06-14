e8d04c5744e74a33a10725bef3990a18b2a6d914
joaolalmeida <joaolalmeida20@gmail.com>
2023-06-14 15:02:05 -0300

    component status

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 74rem;
   min-height: calc(100vh - 10rem);
-  margin: 5rem auto;
+  margin: 4.5rem auto;
   padding: 2.5rem;
 
   background: ${(props) => props.theme['gray-800']};
--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -32,13 +32,11 @@ export const HistoryList = styled.div`
       line-height: 1.6;
 
       &:first-child {
-        //Selecionando a primeira th
         border-top-left-radius: 8px;
         padding-left: 1.5rem;
       }
 
       &:last-child {
-        //Selecionando a ultima th
         border-top-right-radius: 8px;
         padding-right: 1.5rem;
       }
@@ -48,19 +46,40 @@ export const HistoryList = styled.div`
       background-color: ${(props) => props.theme['gray-700']};
       border-top: 4px solid ${(props) => props.theme['gray-800']};
       padding: 1rem;
-      font-size: 0.875;
+      font-size: 0.875rem;
       line-height: 1.6;
 
       &:first-child {
-        //Selecionando a primeira th
         width: 50%;
         padding-left: 1.5rem;
       }
 
       &:last-child {
-        //Selecionando a ultima th
         padding-right: 1.5rem;
       }
     }
+  }
+`
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
